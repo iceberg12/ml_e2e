31e4135bd4180aee6cf3a188249bc0e8250ededd
minhnguyen <tuanminh.nus@gmail.com>
2023-07-23 06:51:34 +0200

    set up package and dependencies

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements(file_path: str) -> List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n', "") for req in requirements]
+
+setup(
+    name='ml_e2e',
+    version='0.0.1',
+    author='Minh',
+    author_email='nminhptnk2@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt') #['pandas', 'numpy', 'seaborn']
+)
